9bca3bf4ffe2851203c55bfe675a182e5645e3dd
Louise-amelie MARECHAL <lmarecha@e1r4p2.42.fr>
2022-06-06 17:43:34 +0200

    pseudo code on going

---
+++ main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/06 17:15:38 by lmarecha          #+#    #+#             */
+/*   Updated: 2022/06/06 17:43:13 by lmarecha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+int	main(int argc, char **argv)
+{
+//		----------------------PARSER---------------
+//		1. verifier que je n'ai pas trop d'arguments
+//		2. verifier si il y a 'number_of_times_each_philosopher_must_eat'
+//			-> ajuster le code en fonction
+//		3. recuperer les donnees de chaque arguments
+//		4. transformer les time_to en milliseconds
+//		5. initialiser la structure
+
+// 		--------------------EXECUTOR--------------------
+// 		THREAD
+// 			->creer un thread par philosophers
+
+// 		EAT
+//		 1. Verifier si le philosopher peut manger
+//			 -> philosophers paires commencent par la fourchette de droite
+//			 -> philosophers impaires prennent la fourchette de gauche first
+//				 si l'une des deux n'est pas dispo il ne peut pas manger
+//
+//		 2. si le philosophe peut manger il mange sinon il continue de penser
+//			 -> si un philo n'a pas commence a manger time_to_die millisec apres le debut de son precedent repas ou du debut de la similuation il meurt
+//			 -> il finit de manger time_to_eat millisec apres le debut de son repas
+//
+//		 SLEEP
+//		 Quand le philosopher a fini de manger il se met a dormir
+//			 -> il dort time_to_sleep millisec apres la fin de son repas (ou apres le debut de sa sieste)
+//
+//		 THINK
+//		 Quand le philosophe a fini de dormir il se met a penser
+//			-> il pense jusqu'a ce qu'il puisse manger || qu'il meurt
+//
+//		 PROMPT INFO
+//		 Des qu'un philosopher fait une action l'ecrire sur le prompt
+//			 -> avec l'actual timestamp auquel l'action a eu lieu
+}
--- milliseconds.c
+++ milliseconds.c
@@ -14,20 +14,6 @@ long int	diff_time_in_msec(struct timeval t1, struct timeval t2)
 
 int	main(void)
 {
-	// struct timeval current_time; // structure definit par sys.time.h
-	// int	start_time;
-	// int	action_time;
-	//
-	// start_time_ms = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
-	// gettimeofday(&current_time, NULL);
-	// printf("seconds = %ld\nmicro seconds = %ld\n", current_time.tv_sec, current_time.tv_usec);
-	// printf("seconds since 1970 en microsecond = %ld\n", current_time.tv_sec * 1000000);
-	// printf("seconds + micro = %ld\n", (current_time.tv_sec * 1000000)+ current_time.tv_usec);
-	// printf("millisecond since 1970 = %ld\n", ((current_time.tv_sec * 1000000)+ current_time.tv_usec) / 1000);
-
-	// ((start_sec - act_sec) * 1000) + ((start_microsec - act_microsec) / 1000)
-	// 1 microsecond == 0,001 millisecond
-
 	struct timeval t1;
 	struct timeval t2;
 

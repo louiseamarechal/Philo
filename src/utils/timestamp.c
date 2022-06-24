64410e4398e56bbf93c6875c9dfa9ee095480047
louisea <louiseamelie2@gmail.com>
2022-06-24 11:10:01 +0200

    usleep fonctionne pas

--- src/utils/timestamp.c
+++ src/utils/timestamp.c
@@ -2,12 +2,12 @@
 
 static long long int	diff_time_in_msec(long long int t1, long long int t2)
 {
-	long long int diff;
-
-	diff = t2 - t1;
+	// long long int diff;
+	//
+	// diff = t2 - t1;
 	// diff += (t1.tv_sec - t2.tv_sec) * 1000;
 	// diff += (t1.tv_usec - t2.tv_usec) / 1000;
-	return (diff);
+	return (t2 - t1);
 }
 
 long long int	timestamp(void)
@@ -27,11 +27,15 @@ void	sleep_state(t_args *args, long long received_time)
 	long long i;
 
 	i = timestamp();
-	while (!(args->died))
+	while (args->died != 1)
 	{
 		if (diff_time_in_msec(i, timestamp()) >= received_time)
 			break ;
 		usleep(50);
+		// printf("i = %lld\n", i);
+		// printf("timestamp = %lld\n", timestamp());
+		// printf("diff = %lld\n", diff_time_in_msec(i, timestamp()));
+		// printf("recevied_time = %lld\n", received_time);
 	}
 }
 

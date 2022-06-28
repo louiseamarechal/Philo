14969a79be0816fe3748eb0e04fb1d415f097836
louisea <louiseamelie2@gmail.com>
2022-06-28 15:18:14 +0200

    Exception 1 & 200 philo ajoutes/modifies

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 17:15:38 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/06/28 10:24:15 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/06/28 15:16:53 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ int	main(int argc, char **argv)
 		printf("Wrong number of arguments !");
 		return (1);
 	}
+	if (ft_atoi(argv[1]) > 200)
+		return (1);
 	initialize(&args, argv, argc);
-	if (args.nb_philo < 2)
+	if (args.nb_philo == 1)
 	{
 			print_state(&args, 1, "take a fork");
 			sleep_mode(&args, args.t_die);
 			print_state(&args, 1, "died");
 			args.died = 1;
 	}
-	if (args.nb_philo > 200)
-		return (1);
-	executor(&args);
-
+	else
+		executor(&args);
 	return (0);
 }

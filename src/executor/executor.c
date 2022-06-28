166c208a31554ee97f5705f13f27dd6825ae0ad5
louisea <louiseamelie2@gmail.com>
2022-06-28 11:42:55 +0200

    check des morts et fin de repas sur un thread general au lieu de sur chaque thread philo

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 12:32:49 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/06/27 18:27:23 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/06/28 11:42:04 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 //
 void	philosopher_eats(t_args *args, int id)
 {
-	if (args->philosophers[id].nb_meal == args->number_must_eat || args->died == 1)
+	if (args->all_ate == 1 || args->died == 1)
 		return ;
 	if (id % 2 == 0)
 	{
@@ -36,43 +36,9 @@ void	philosopher_eats(t_args *args, int id)
 	sleep_mode(args, args->t_eat);
 	pthread_mutex_unlock(&args->forks[args->philosophers[id].right_fork]);
 	pthread_mutex_unlock(&args->forks[args->philosophers[id].left_fork]);
-	// pthread_mutex_lock(&args->print_philo_state);
-	// printf("\033[0;33mphilo %d ate %d times \033[0m\n", id, args->philosophers[id].nb_meal);
-	// pthread_mutex_unlock(&args->print_philo_state);
-}
-
-
-void	is_anyone_dead_or_full(t_args *args, int id)
-{
-//-> si un philo n'a pas commence a manger time_to_die millisec apres le debut de son precedent repas ou du debut de la similuation il meurt
 	pthread_mutex_lock(&args->print_philo_state);
-	printf("t_die = %d\n", args->t_die);
-	printf("philo[%d] timestamp = %lld\n", id, timestamp());
-	printf("first timestamp = %lld\n", args->first_timestamp);
-	printf("philo[%d].started_meal = %lld\n", id, args->philosophers[id].started_meal);
-	printf("timestamp - started_meal = %lld\n", diff_time_in_msec(args->philosophers[id].started_meal, timestamp()));
-	// printf("started_meal - first_timestamp = %lld\n", diff_time_in_msec(args->first_timestamp, args->philosophers[id].started_meal));
+	printf("\033[0;33mphilo %d ate %d times \033[0m\n", id, args->philosophers[id].nb_meal);
 	pthread_mutex_unlock(&args->print_philo_state);
-	if (args->number_must_eat != -1 && args->philosophers[id].nb_meal == args->number_must_eat)
-	{
-		pthread_mutex_lock(&args->meal_state);
-		args->all_ate += 1;
-		pthread_mutex_unlock(&args->meal_state);
-	}
-	//pb ici ?
-	// if (args->number_must_eat != -1 && args->philosophers[id].nb_meal < args->number_must_eat)
-	// {
-	if (diff_time_in_msec(args->philosophers[id].started_meal, timestamp()) > args->t_die)
-	{
-		print_state(args, id, "died waited too long since last meal");
-		args->died = 1;
-	}
-	if (args->philosophers[id].nb_meal == 1 && diff_time_in_msec(args->first_timestamp, args->philosophers[id].started_meal) > args->t_die)
-	{
-		print_state(args, id, "died haven't eaten since begining");
-		args->died = 1;
-	}
-	// }
 }
 
 static void	*print_thread(void *philo)
@@ -82,12 +48,11 @@ static void	*print_thread(void *philo)
 
 	philosopher = (t_philosopher *)philo;
 	args = philosopher->args;
-	while (args->died != 1 && philosopher->nb_meal != args->number_must_eat)
+	while (args->died != 1)
 	{
 		philosopher_eats(args, philosopher->id);
-		is_anyone_dead_or_full(args, philosopher->id);
-		// if (args->all_ate >= args->nb_philo)
-		// 	break ;
+		if (args->all_ate == 1)
+			break ;
 		print_state(args, philosopher->id, "is sleeping");
 		sleep_mode(args, args->t_sleep);
 		print_state(args, philosopher->id, "is thinking");
@@ -96,14 +61,42 @@ static void	*print_thread(void *philo)
 	return (NULL);
 }
 
+void	is_anyone_dead_or_full(t_args *args, t_philosopher *philo)
+{
+	int	id;
+
+	id = 0;
+	while (args->all_ate != 1)
+	{
+		id = 1;
+		while (args->died != 1 && id <= args->nb_philo)
+		{
+			pthread_mutex_lock(&args->meal_state);
+			if (diff_time_in_msec(philo[id].started_meal, timestamp()) > args->t_die)
+			{
+				print_state(args, id, "died");
+				args->died = 1;
+			}
+			pthread_mutex_unlock(&args->meal_state);
+			id++;
+		}
+		if (args->died == 1)
+			break;
+		id = 1;
+		while (id <= args->nb_philo && philo[id].nb_meal == args->number_must_eat)
+			id++;
+		if (id == args->nb_philo)
+			args->all_ate = 1;
+		usleep(1000);
+	}
+}
+
 int	executor(t_args *args)
 {
 	int				i;
-	int				max_meals;
 	t_philosopher	*philo;
 
 	i = 1;
-	max_meals = 0;
 	philo = args->philosophers;
 	while (i <= args->nb_philo)
 	{
@@ -111,13 +104,13 @@ int	executor(t_args *args)
 			return (0);
 		i++;
 	}
-	i = 0;
-	while (i < args->nb_philo)
+	is_anyone_dead_or_full(args, philo);
+	i = 1;
+	while (i <= args->nb_philo)
 	{
 		pthread_join(philo[i].thread_id, NULL);
 		i++;
 	}
-	// philo_death_checker();
 	// destroy_all();
 	return (1);
 	// je cree les thread de chaque philosophe qui m'envoit vers la fonction qui s'execute dans le thread
--- src/parser/initializer.c
+++ src/parser/initializer.c
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 08:43:37 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/06/27 11:13:46 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/06/28 10:17:28 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static int	initialize_philo(t_args *args, t_philosopher *philo)
 			philo[i].left_fork = (i + 1) % args->nb_philo; // pour que le dernier philo prenne la fourchette du premier
 		else
 			philo[i].left_fork = i + 1;
-		philo[i].started_meal = 0;
+		philo[i].started_meal = args->first_timestamp;
 		philo[i].args = args;
 		i++;
 	}
@@ -53,8 +53,6 @@ static int	initialize_philo(t_args *args, t_philosopher *philo)
 int	initialize(t_args *args, char **argv, int argc)
 {
 	args->nb_philo = ft_atoi(argv[1]);
-	if (args->nb_philo < 2 || args->nb_philo > 200) // check le nb de philosophers
-		return (0); //erreur
 	args->t_die = ft_atoi(argv[2]);
 	args->t_eat = ft_atoi(argv[3]);
 	args->t_sleep = ft_atoi(argv[4]);
@@ -69,5 +67,7 @@ int	initialize(t_args *args, char **argv, int argc)
 	args->first_timestamp = timestamp();
 	initialize_philo(args, args->philosophers);
 	intitialize_mutex(args);
+	if (args->nb_philo < 2 || args->nb_philo > 200) // check le nb de philosophers
+		return (0); //erreur
 	return (1);
 }

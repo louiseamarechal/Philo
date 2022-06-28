fff604df305ac7cdbbec656bbfc372bf0f18aa5e
louisea <louiseamelie2@gmail.com>
2022-06-28 15:25:08 +0200

    anyone_dead_or_full update pour regler le bug 5 800 200 200 7 messages has taken a fork remis sur les mutex pour eviter de le print si  le mutex est lock

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   executor.c                                         :+:      :+:    :+:   */
+/*   executor2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 12:32:49 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/06/28 11:42:04 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/06/28 15:03:28 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
-//
+
 void	philosopher_eats(t_args *args, int id)
 {
 	if (args->all_ate == 1 || args->died == 1)
@@ -19,15 +19,17 @@ void	philosopher_eats(t_args *args, int id)
 	if (id % 2 == 0)
 	{
 		pthread_mutex_lock(&args->forks[args->philosophers[id].right_fork]);
+		print_state(args, id, "has taken a fork");
 		pthread_mutex_lock(&args->forks[args->philosophers[id].left_fork]);
+		print_state(args, id, "has taken a fork");
 	}
 	else
 	{
 		pthread_mutex_lock(&args->forks[args->philosophers[id].left_fork]);
+		print_state(args, id, "has taken a fork");
 		pthread_mutex_lock(&args->forks[args->philosophers[id].right_fork]);
+		print_state(args, id, "has taken a fork");
 	}
-	print_state(args, id, "has taken a fork");
-	print_state(args, id, "has taken a fork");
 	print_state(args, id, "is eating");
 	pthread_mutex_lock(&args->meal_state);
 	args->philosophers[id].started_meal = timestamp();
@@ -36,9 +38,9 @@ void	philosopher_eats(t_args *args, int id)
 	sleep_mode(args, args->t_eat);
 	pthread_mutex_unlock(&args->forks[args->philosophers[id].right_fork]);
 	pthread_mutex_unlock(&args->forks[args->philosophers[id].left_fork]);
-	pthread_mutex_lock(&args->print_philo_state);
-	printf("\033[0;33mphilo %d ate %d times \033[0m\n", id, args->philosophers[id].nb_meal);
-	pthread_mutex_unlock(&args->print_philo_state);
+	// pthread_mutex_lock(&args->print_philo_state);
+	// printf("\033[0;33mphilo %d ate %d times \033[0m\n", id, args->philosophers[id].nb_meal);
+	// pthread_mutex_unlock(&args->print_philo_state);
 }
 
 static void	*print_thread(void *philo)
@@ -48,45 +50,45 @@ static void	*print_thread(void *philo)
 
 	philosopher = (t_philosopher *)philo;
 	args = philosopher->args;
-	while (args->died != 1)
+	if (philosopher->id % 2 == 0)
+		usleep(100);
+	while (args->died != 1 && args->all_ate != 1)
 	{
 		philosopher_eats(args, philosopher->id);
-		if (args->all_ate == 1)
-			break ;
 		print_state(args, philosopher->id, "is sleeping");
 		sleep_mode(args, args->t_sleep);
 		print_state(args, philosopher->id, "is thinking");
-
 	}
 	return (NULL);
 }
 
 void	is_anyone_dead_or_full(t_args *args, t_philosopher *philo)
 {
-	int	id;
+	int	i;
 
-	id = 0;
 	while (args->all_ate != 1)
 	{
-		id = 1;
-		while (args->died != 1 && id <= args->nb_philo)
+		i = 0;
+		while (args->died != 1 && i < args->nb_philo)
 		{
 			pthread_mutex_lock(&args->meal_state);
-			if (diff_time_in_msec(philo[id].started_meal, timestamp()) > args->t_die)
+			if (diff_time_in_msec(philo[i].started_meal, timestamp()) > args->t_die)
 			{
-				print_state(args, id, "died");
+				print_state(args, i, "\033[31mdied\033[0m");
 				args->died = 1;
 			}
 			pthread_mutex_unlock(&args->meal_state);
-			id++;
+			i++;
 		}
 		if (args->died == 1)
 			break;
-		id = 1;
-		while (id <= args->nb_philo && philo[id].nb_meal == args->number_must_eat)
-			id++;
-		if (id == args->nb_philo)
+		i = 0;
+		pthread_mutex_lock(&args->meal_state);
+		while (args->number_must_eat != -1 && i < args->nb_philo && philo[i].nb_meal >= args->number_must_eat)
+			i++;
+		if (i == args->nb_philo)
 			args->all_ate = 1;
+		pthread_mutex_unlock(&args->meal_state);
 		usleep(1000);
 	}
 }
@@ -96,22 +98,23 @@ int	executor(t_args *args)
 	int				i;
 	t_philosopher	*philo;
 
-	i = 1;
+	i = 0;
 	philo = args->philosophers;
-	while (i <= args->nb_philo)
+	while (i < args->nb_philo)
 	{
+		printf("Create thread %d\n", i);
 		if (pthread_create(&(philo[i].thread_id), NULL, print_thread, &(philo[i])) != 0)
 			return (0);
 		i++;
 	}
 	is_anyone_dead_or_full(args, philo);
-	i = 1;
-	while (i <= args->nb_philo)
+	i = 0;
+	while (i < args->nb_philo)
 	{
 		pthread_join(philo[i].thread_id, NULL);
 		i++;
 	}
-	// destroy_all();
+	kill_everything(args);
 	return (1);
 	// je cree les thread de chaque philosophe qui m'envoit vers la fonction qui s'execute dans le thread
 	// je check si quelu'un doit mourir
